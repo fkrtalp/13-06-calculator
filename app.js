4e3c23ae6ed1f420373da582e163891a36ccdf58
Fikret ALPSOY <fikretalpsoy90@gmail.com>
2023-06-13 14:51:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const previous = document.querySelector(".previous-display");
+const current = document.querySelector(".current-display");
+const button = document.querySelector(".buttons-container");
+// ! global alan
+let üstsatir = "";
+let altsatir = "";
+let operator = "";
+
+button.addEventListener("click", (e) => {
+  if (e.target.classList.contains("num")) {
+    sayiyiAl(e.target.textContent);
+    yazdir();
+  }
+  if (e.target.classList.contains("operator")) {
+    choose(e.target.textContent)
+    yazdir();
+  }
+  if (e.target.classList.contains("equal")) {
+    hesapla()
+    yazdir();
+  }
+  if (e.target.classList.contains("pm")) {
+    if(!altsatir)return;
+    altsatir *= -1
+    yazdir();
+  }
+  if (e.target.classList.contains("percent")) {
+    if(!altsatir)return;
+    altsatir = altsatir / 100
+    yazdir();
+  }
+  if (e.target.classList.contains("ac")) {
+    altsatir =""
+    üstsatir =""
+    operator =""
+    yazdir();
+  }
+
+});
+
+const sayiyiAl = (num) => {
+  if(num === "0" && altsatir==="0") return
+  if(num !== "." && altsatir==="0") {
+    altsatir = num
+    return
+  }
+  
+  if(num === "." && altsatir.includes(".")) return
+  if(altsatir.length > 10)return
+  altsatir += num;
+    
+
+};
+
+const yazdir = () => {
+  previous.textContent = `${üstsatir}${operator}`;
+  current.textContent = altsatir;
+};
+
+const choose = (op) =>{
+    operator = op
+    üstsatir = altsatir
+    altsatir =""
+
+}
+
+const hesapla = ()=>{
+    let calculation = 0
+    const sayi1 =Number(üstsatir)
+    const sayi2 =Number(altsatir)
+    console.log(sayi1);
+    console.log(sayi2);
+    switch (operator) {
+        case "+":
+            calculation = sayi1 + sayi2
+            console.log(calculation);
+            break;
+        case "-":
+            calculation = sayi1 - sayi2
+            break;
+        case "x":
+            calculation = sayi1 * sayi2
+            break;
+        case "÷":
+            calculation = sayi1 / sayi2
+            break;
+    
+        default:
+            return;
+    }
+altsatir =calculation
+üstsatir = ""
+operator=""
+}
